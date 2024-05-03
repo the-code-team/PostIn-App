71cc346115e44f9c675f381790c2e95d5b82eaac
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-03 13:27:27 +0200

    Mapa

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+// Tutorial: https://www.youtube.com/watch?v=ZnZM8ot5lcc
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Mapa',
-        style: TextStyle(fontSize: 24.0),
-      ),
-    );
+    return FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(38.699575, -0.474774),
+          initialZoom: 11,
+          interactionOptions:
+              InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom),
+        ),
+        children: [
+          openStreetMapTileLayer,
+        ]);
   }
 }
+
+TileLayer get openStreetMapTileLayer => TileLayer(
+      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      userAgentPackageName: 'dev.fleaflet.flutter_map.exemple',
+    );
