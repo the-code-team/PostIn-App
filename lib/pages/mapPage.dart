bad53e3538b44beb3f831a0823abf6a15ee5fb28
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-03 13:40:09 +0200

    MapPage: boton

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -3,20 +3,28 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 // Tutorial: https://www.youtube.com/watch?v=ZnZM8ot5lcc
+// Documentación oficial: https://docs-fleaflet-dev.translate.goog/?_x_tr_sl=auto&_x_tr_tl=es&_x_tr_hl=es&_x_tr_hist=true
+// https://pub.dev/packages/flutter_map/example
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-        options: MapOptions(
-          initialCenter: LatLng(38.699575, -0.474774),
-          initialZoom: 11,
-          interactionOptions:
-              InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom),
-        ),
-        children: [
-          openStreetMapTileLayer,
-        ]);
+    return Scaffold(
+      body: FlutterMap(
+          options: MapOptions(
+            initialCenter: LatLng(38.699575, -0.474774),
+            initialZoom: 11,
+            interactionOptions:
+                InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom),
+          ),
+          children: [
+            openStreetMapTileLayer,
+          ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {},
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+    );
   }
 }
 
