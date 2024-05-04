80dd0350b6a06fc4cffc914c5a25025c1127c4b7
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-04 13:24:40 +0200

    feat: Color change
    I have changed the color of the button and the search bar

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        primaryColor: Colors.black,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
