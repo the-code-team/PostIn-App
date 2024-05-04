895ba7359f3083879482fde7155c878d15659e2b
inesjurado <inesjurado2@gmail.com>
2024-05-04 19:25:38 +0200

    feat(posin): modificacion

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Perfil',
-        style: TextStyle(fontSize: 24.0),
     return Scaffold(
       appBar: AppBar(
         ///automaticallyImplyLeading: false,
