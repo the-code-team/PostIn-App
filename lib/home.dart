2e39df355b1c11a4d7b2c4ef26dcbdc38de57539
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-04 12:13:54 +0200

    feat:buttons added to bottom bar

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:postin_app/pages/mapPage.dart';
 import 'package:postin_app/pages/eventsPage.dart';
 import 'package:postin_app/pages/profilePage.dart';
+import 'package:postin_app/pages/groupsPage.dart';
+import 'package:postin_app/pages/proposalsPage.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
@@ -15,7 +17,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _paginaActual = 0;
 
-  List<Widget> _paginas = [MapPage(), EventsPage(), ProfilePage()];
+  List<Widget> _paginas = [
+    MapPage(),
+    ProposalsPage(),
+    EventsPage(),
+    GroupsPage(),
+    ProfilePage()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +38,11 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.map), label: "Map"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.edit_location_alt_outlined), label: "Proposes"),
           BottomNavigationBarItem(icon: Icon(Icons.event), label: "Eventos"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble_outline), label: "Groups"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Perfil")
         ],
       ),
