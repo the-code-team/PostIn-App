2764d202ef50c4f4924c08b6952cf78c9b96a771
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-05 13:14:19 +0200

    refactor: mapComponent
    The map has been converted to a component

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:postin_app/components/search_bar/searchBar.dart';
+import 'package:postin_app/components/map/map.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -20,20 +19,7 @@ class MapPage extends StatelessWidget {
         children: [
           buildSearchField(context),
           // The expanded helps us so that the FlutterMap is not infinite. If the FlutterMap is infinite it cannot be placed in a column
-          Expanded(
-            child: FlutterMap(
-              options: const MapOptions(
-                initialCenter: LatLng(38.699575, -0.474774),
-                initialZoom: 11,
-                interactionOptions: InteractionOptions(
-                  flags: ~InteractiveFlag.doubleTapDragZoom,
-                ),
-              ),
-              children: [
-                openStreetMapTileLayer,
-              ],
-            ),
-          ),
+          Expanded(child: MapComponent()),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -44,9 +30,4 @@ class MapPage extends StatelessWidget {
       ),
     );
   }
-
-  TileLayer get openStreetMapTileLayer => TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'dev.fleaflet.flutter_map.exemple',
-      );
 }
