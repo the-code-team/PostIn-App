e737839b86af2c6e7c5688abc57c39586182e0a2
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-05 13:14:19 +0200

    refactor: mapComponent
    The map has been converted to a component

---
+++ lib/components/map/map.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+class MapComponent extends StatelessWidget {
+  const MapComponent({Key? key});
+
+  TileLayer get openStreetMapTileLayer => TileLayer(
+        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        userAgentPackageName: 'dev.fleaflet.flutter_map.exemple',
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(38.699575, -0.474774),
+        initialZoom: 11,
+        interactionOptions: InteractionOptions(
+          flags: ~InteractiveFlag.doubleTapDragZoom,
+        ),
+      ),
+      children: [
+        openStreetMapTileLayer,
+      ],
+    );
+  }
+}
