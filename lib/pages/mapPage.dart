774d3d35c565422a15e14e22c1c53cab07aa6019
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-05 12:10:20 +0200

    refactor: mapPage, SearchBar
    Features have been optimized

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -4,6 +4,8 @@ import 'package:latlong2/latlong.dart';
 import 'package:postin_app/components/search_bar/searchBar.dart';
 
 class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +15,7 @@ class MapPage extends StatelessWidget {
           // The expanded helps us so that the FlutterMap is not infinite. If the FlutterMap is infinite it cannot be placed in a column
           Expanded(
             child: FlutterMap(
-              options: MapOptions(
+              options: const MapOptions(
                 initialCenter: LatLng(38.699575, -0.474774),
                 initialZoom: 11,
                 interactionOptions: InteractionOptions(
@@ -30,8 +32,8 @@ class MapPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {},
         backgroundColor: Theme.of(context).primaryColor,
+        shape: const CircleBorder(),
         child: const Icon(Icons.add, color: Colors.white),
-        shape: CircleBorder(),
       ),
     );
   }
