bb54d9385b05cc7052552fc7229656b94aa37470
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-06 20:25:15 +0200

    feat: filtre dropdown

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -13,20 +13,30 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   int selectedIndex = 0; // State that stores the index of the selected item
+  bool isEventListVisible = false; // State to control the visibility of EventListWidget
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          buildTitleBar(context),
-          EventListWidget(
-            selectedIndex: selectedIndex,
-            onTap: (index) {
-              setState(() {
-                selectedIndex = index;
-              });
-            },
+          buildTitleBar(context, () {
+            setState(() {
+              isEventListVisible = !isEventListVisible; // Toggle visibility of EventListWidget
+            });
+          }),
+          AnimatedCrossFade(
+            duration: const Duration(milliseconds: 300),
+            crossFadeState: isEventListVisible ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+            firstChild: Container(), // Container for collapsed state
+            secondChild: EventListWidget( // EventListWidget for expanded state
+              selectedIndex: selectedIndex,
+              onTap: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
           ),
           Expanded(
             child: MapComponent(),
