c50afccebae341ba03b36ecb56779dd0aef9ee84
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-12 17:53:45 +0200

    feat: barFilter

--- lib/components/titles/titleBar.dart
+++ lib/components/titles/titleBar.dart
@@ -3,17 +3,10 @@ import 'package:flutter/material.dart';
 Widget buildTitleBar(BuildContext context, VoidCallback onFilterPressed) {
   return Padding(
     padding: const EdgeInsets.fromLTRB(16, 35, 16, 15),
-    child: Container(
-      padding: EdgeInsets.fromLTRB(25, 2, 5, 2),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 225, 225, 225),
-        border: Border.all(
-          color: Theme.of(context).primaryColor,
-          width: 1,
-        ),
-        borderRadius: BorderRadius.circular(40),
-      ),
+    child: ElevatedButton(
+      onPressed: () {
+        // Acción
+      },
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -23,13 +16,9 @@ Widget buildTitleBar(BuildContext context, VoidCallback onFilterPressed) {
               fontSize: 18,
             ),
           ),
-          TextButton(
-            onPressed:
-                onFilterPressed, // Llama a la función de callback cuando se presiona el botón de filtro
-            child: Icon(
-              Icons.filter_list_outlined,
-              color: Theme.of(context).primaryColor,
-            ),
+          Icon(
+            Icons.filter_list_outlined,
+            color: Theme.of(context).primaryColor,
           ),
         ],
       ),
