5743ac82a1296c713294dd794ef857b292b97103
marcosvicmar <marcosvicmart@gmail.com>
2024-05-27 01:23:33 +0200

    Feat: Slide Cards

---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final String imageUrl;
+  final String name;
+  final int age;
+  final String description;
+  final List<String> interests;
+
+  User(this.imageUrl, this.name, this.age, this.description, this.interests);
+}
