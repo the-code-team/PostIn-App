6e0a4ca3d4892239a084b4f24e955e42424be1e1
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-27 19:34:38 +0200

    feat: subir fotos

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 import 'package:postin_app/components/map/map.dart';
 
 class MapPage extends StatefulWidget {
@@ -9,21 +12,23 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  int selectedIndex = 0; // State that stores the index of the selected item
+  int selectedIndex =
+      0; // Estado que almacena el índice del elemento seleccionado
   bool isEventListVisible =
-      false; // State to control the visibility of EventListWidget
+      false; // Estado para controlar la visibilidad de EventListWidget
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: MapComponent(),
-            ),
-          ],
-        ),
-        floatingActionButton: addEventFloatingActionButton(context));
+      body: Column(
+        children: [
+          Expanded(
+            child: MapComponent(),
+          ),
+        ],
+      ),
+      floatingActionButton: addEventFloatingActionButton(context),
+    );
   }
 }
 
@@ -39,52 +44,81 @@ Widget addEventFloatingActionButton(BuildContext context) {
 void showAddEventDialog(BuildContext context) {
   final _titleController = TextEditingController();
   final _descriptionController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  List<File> _images = [];
 
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Agregar Evento'),
-        content: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: 'Título'),
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          Future<void> _pickImage() async {
+            final pickedFile =
+                await _picker.pickImage(source: ImageSource.gallery);
+            if (pickedFile != null) {
+              setState(() {
+                _images.add(File(pickedFile.path));
+              });
+            }
+          }
+
+          return AlertDialog(
+            title: Text('Agregar Evento'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(labelText: 'Título'),
+                  ),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(labelText: 'Descripción'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: _pickImage,
+                    icon: Icon(Icons.photo),
+                    label: Text('Subir Fotos'),
+                  ),
+                  SizedBox(height: 10),
+                  _images.isNotEmpty
+                      ? Wrap(
+                          spacing: 8.0,
+                          runSpacing: 8.0,
+                          children: _images.map((image) {
+                            return Image.file(
+                              image,
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            );
+                          }).toList(),
+                        )
+                      : Text('No se han seleccionado fotos'),
+                ],
               ),
-              TextField(
-                controller: _descriptionController,
-                decoration: InputDecoration(labelText: 'Descripción'),
-              ),
-              ElevatedButton.icon(
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Cancelar'),
                 onPressed: () {
-                  // Lógica para subir fotos
+                  Navigator.of(context).pop();
                 },
-                icon: Icon(Icons.photo),
-                label: Text('Subir Fotos'),
+              ),
+              ElevatedButton(
+                child: Text('Guardar'),
+                onPressed: () {
+                  // Lógica para guardar el evento
+                  String title = _titleController.text;
+                  String description = _descriptionController.text;
+                  print('Título: $title, Descripción: $description');
+                  // Aquí puedes manejar las imágenes (_images) como necesites
+                  Navigator.of(context).pop();
+                },
               ),
             ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: Text('Cancelar'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          ElevatedButton(
-            child: Text('Guardar'),
-            onPressed: () {
-              // Lógica para guardar el evento
-              // Por ejemplo, podrías recoger los valores de los controladores y almacenarlos en una base de datos o lista
-              String title = _titleController.text;
-              String description = _descriptionController.text;
-              print('Título: $title, Descripción: $description');
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
+          );
+        },
       );
     },
   );
