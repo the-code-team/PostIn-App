b89843b9665a6ae8bb285bba4239484bb680ed61
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-06-10 15:40:03 +0200

    feat: Bookmarks loaded from json

--- lib/components/map/map.dart
+++ lib/components/map/map.dart
@@ -1,8 +1,9 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:postin_app/components/titles/titleBar.dart';
-
 import 'package:postin_app/pages/eventDetailsPageAddEvent.dart';
 
 class LocationEvent {
@@ -10,10 +11,38 @@ class LocationEvent {
   final String identifier;
 
   LocationEvent({required this.coordinates, required this.identifier});
+
+  factory LocationEvent.fromJson(Map<String, dynamic> json) {
+    return LocationEvent(
+      coordinates: LatLng(
+          json['coordinates']['latitude'], json['coordinates']['longitude']),
+      identifier: json['identifier'],
+    );
+  }
 }
 
-class MapComponent extends StatelessWidget {
-  const MapComponent({Key? key});
+class MapComponent extends StatefulWidget {
+  const MapComponent({Key? key}) : super(key: key);
+
+  @override
+  _MapComponentState createState() => _MapComponentState();
+}
+
+class _MapComponentState extends State<MapComponent> {
+  late Future<List<LocationEvent>> locationEventsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    locationEventsFuture = loadLocationEvents();
+  }
+
+  Future<List<LocationEvent>> loadLocationEvents() async {
+    final String response =
+        await rootBundle.loadString('assets/location_events.json');
+    final List<dynamic> data = json.decode(response);
+    return data.map((json) => LocationEvent.fromJson(json)).toList();
+  }
 
   TileLayer get openStreetMapTileLayer => TileLayer(
         urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -51,26 +80,31 @@ class MapComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Ejemplo de lista de eventos de ubicación
-    final locationEvents = [
-      LocationEvent(
-          coordinates: LatLng(38.699575, -0.474774), identifier: 'ID1'),
-      LocationEvent(
-          coordinates: LatLng(38.699575, -0.471774), identifier: 'ID2'),
-    ];
-
-    return FlutterMap(
-      options: const MapOptions(
-        center: LatLng(38.699575, -0.474774),
-        zoom: 11,
-        interactiveFlags:
-            InteractiveFlag.all & ~InteractiveFlag.doubleTapDragZoom,
-      ),
-      children: [
-        openStreetMapTileLayer,
-        MarkerLayer(markers: getMarkers(context, locationEvents)),
-        buildTitleBar(context),
-      ],
+    return FutureBuilder<List<LocationEvent>>(
+      future: locationEventsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(child: Text('No events found.'));
+        } else {
+          return FlutterMap(
+            options: const MapOptions(
+              center: LatLng(38.699575, -0.474774),
+              zoom: 11,
+              interactiveFlags:
+                  InteractiveFlag.all & ~InteractiveFlag.doubleTapDragZoom,
+            ),
+            children: [
+              openStreetMapTileLayer,
+              MarkerLayer(markers: getMarkers(context, snapshot.data!)),
+              buildTitleBar(context),
+            ],
+          );
+        }
+      },
     );
   }
 }
